173eba0a8ff4f9f8b0b39d4c5c29f598a886ae6d
andinenovia <andinenovia12@gmail.com>
2026-09-19 18:51:58 +0700

    pertemuan 3

---
+++ app/Http/Controllers/mahasiswaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mahasiswaController extends Controller
+{
+    //
+    public function index()
+    {
+        $mahasiswa = [
+            'nim' => '251011700304',
+            'nama' => 'Andine Novia Azizah',
+            'prodi' => 'Sistem Informasi',
+            'kampus' => 'Universitas Pamulang',
+            'email' => 'andinenovia12@gmail.com',
+            'status' => 'aktif',
+        ];
+
+        return view('mahasiswa', compact('mahasiswa'));
+    }
+}
